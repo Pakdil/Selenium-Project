6b442b5966e3bebba975b57e2275889f555e18e0
pakdil <eaasbb@gmail.com>
2022-09-09 16:36:06 +0300

    day3 T1

---
+++ src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/day2_Linktext_Practice.java
@@ -0,0 +1,55 @@
+package com.cydeo.tests.day2_locators_getText_getAttribute;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class day2_Linktext_Practice {
+
+
+    public static void main(String[] args) throws InterruptedException {
+
+        //1. Opern Chrome browser
+        //1. Open Chrome browser
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        // 2. Go to http://practice.cydeo.com
+
+        driver.navigate().to("http://practice.cydeo.com");
+        // 3. Click to A/B Testing from top of the list
+        //driver.findElement(By.linkText("A/B Testing")).click();
+        WebElement abTestLink = driver.findElement(By.linkText("A/B Testing"));
+        abTestLink.click();
+
+            // 4.Verify title is:
+            // Expected: No A/B Test
+
+            String expectedTitle = "No A/B Test";
+            String actualTitle = driver.getTitle();
+
+            if (actualTitle.equals(expectedTitle)) {
+                System.out.println("Title verification PASS!");
+            }else {
+                System.out.println("Title verification FAILED!!!");
+            }
+            Thread.sleep(3000);
+
+            // 5. Go back to home page by using the .back();
+            driver.navigate().back();
+
+            // 6. Verify title equals
+            // Expected: Practice
+
+            String expectedTitle2 = "Practice";
+            String actualTile2 = driver.getTitle();
+
+            if (actualTile2.equals(expectedTitle2)) {
+                System.out.println("Title verification PASSED!");
+            }else {
+                System.out.println("Title verification FAILED!!!");
+            }
+    }
+}
---
+++ src/test/java/com/cydeo/tests/day3_sccSelector_xpath/T1_locators_getText.java
@@ -0,0 +1,53 @@
+package com.cydeo.tests.day3_sccSelector_xpath;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class T1_locators_getText {
+
+    public static void main(String[] args) {
+
+        /*
+        TC #1: NextBaseCRM, locators and getText() practice
+        1- Open a chrome browser
+        2- Go to: https://login1.nextbasecrm.com/
+        3- Enter incorrect username: “incorrect”
+        4- Enter incorrect password: “incorrect”
+        5- Click to login button.
+        6- Verify error message text is as expected:
+        Expected: Incorrect login or password
+
+         */
+
+        //1- Open a chrome browser
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        // Go to: https://login1.nextbasecrm.com/
+        driver.get("https://login1.nextbasecrm.com/");
+
+        // 3- Enter incorrect username: “incorrect”
+      // driver.findElement(By.className("login-inp")).sendKeys("incorrect");
+        WebElement inputUsername = driver.findElement(By.className("login-inp"));
+        inputUsername.sendKeys("incorrect");
+
+
+       // 4- Enter incorrect password: “incorrect”
+       WebElement inputPassword = driver.findElement(By.name("USER_PASSWORD"));
+       inputPassword.sendKeys("incorrect");
+
+       // 5- Click to login button.
+
+        WebElement login = driver.findElement(By.className("login-btn"));
+        login.click();
+
+
+
+
+    }
+}
