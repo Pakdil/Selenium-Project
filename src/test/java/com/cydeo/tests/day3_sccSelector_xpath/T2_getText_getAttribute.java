eb9de1c8ceb33480b9fbaca7d15b5e64c0fb5b5b
pakdil <eaasbb@gmail.com>
2022-09-13 11:25:18 +0300

    day3 Task2

--- src/test/java/com/cydeo/tests/day3_sccSelector_xpath/T2_getText_getAttribute.java
+++ src/test/java/com/cydeo/tests/day3_sccSelector_xpath/T2_getText_getAttribute.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import java.util.IllegalFormatCodePointException;
+
 public class T2_getText_getAttribute {
 
     public static void main(String[] args) {
@@ -23,10 +25,44 @@ WebElement RememberMeLable = driver.findElement(By.className("login-item-checkbo
 String expectedRememberMeLable = "Remember me on this computer";
 String actualRememberMeLable = RememberMeLable.getText();
 
+
+
+if (actualRememberMeLable.equals(expectedRememberMeLable)) {
+    System.out.println("Lable verificaton PASSED!");
+}else {
+    System.out.println("Lable verification FAILED!!!");
+}
+
 //4- Verify “forgot password” link text is as expected:
 //    Expected: Forgot your password?
+
+        WebElement ForgotPasswordLink = driver.findElement(By.className("login-link-forgot-pass"));
+
+String expectedForgotPasswordLinkText = "Forgot your password?";
+String actualForgotPasswordLinkText = ForgotPasswordLink.getText();
+
+if (actualForgotPasswordLinkText.equals(expectedForgotPasswordLinkText)) {
+    System.out.println("Forgot password link verification PASSED!");
+} else {
+    System.out.println("actualForgotPasswordLinkText" +actualForgotPasswordLinkText);
+    System.out.println("expectedForgotPasswordLinkText = " + expectedForgotPasswordLinkText);
+    System.out.println("Forgot password link verification FAILED!!!");
+}
 //            5- Verify “forgot password” href attribute’s value contains expected:
 //    Expected: forgot_password=yes
+
+        String expectedInHref = "forgot_password=yes";
+String actualHrefAttributeValue = ForgotPasswordLink.getAttribute("href");
+
+        System.out.println("actualHrefAttributeValue = " + actualHrefAttributeValue);
+
+if (actualHrefAttributeValue.contains(expectedInHref)) {
+
+    System.out.println("HREF attribute value verification PASSED!");
+} else {
+
+    System.out.println("HREF attribute value verification FAILED!!!");
+}
 
 
 
