ad969057bd77954b3b452b79290ea4cfe6c33093
pakdil <eaasbb@gmail.com>
2022-10-06 00:37:04 +0300

    javascript executor

--- src/test/java/com/cydeo/tests/JSExecuter.java
+++ src/test/java/com/cydeo/tests/JSExecuter.java
@@ -1,6 +1,38 @@
 package com.cydeo.tests;
 
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
 public class JSExecuter {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUpMethod() {
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+    }
+    @Test
+    public void scrollingTest() throws InterruptedException {
+
+       driver.get("http://practice.cydeo.com/");
+
+        WebElement cydeoLink = driver.findElement(By.linkText("CYDEO"));
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+
+        Thread.sleep(3000);
+        js.executeScript("arguments[0].scrollIntoView(true);",cydeoLink);
+        Thread.sleep(3000);
+        js.executeScript("arguments[0].click();", cydeoLink);
+
+        }
+
 
 
 }
